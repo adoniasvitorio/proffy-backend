01b232df2c95d07a7f75814338d32c4f3b94835d
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-08-05 21:37:36 -0300

    create my hello world

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,9 @@
-console.log('Hello world');
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    return response.json('hello world');
+})
+
+app.listen(3333);
