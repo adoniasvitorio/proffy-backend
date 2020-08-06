af3ce485a7198a4916d7ccdc878ff1d6ceaaeb66
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-08-06 08:59:45 -0300

    create total connections migration, controller, database and route

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+
 import ClassesControler from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 
 const routes = express.Router();
 const classesControllers = new ClassesControler();
+const connectionsController = new ConnectionsController();
 
 routes.get('/', (request, response) => {
     return response.json({ message: 'hello world' });
@@ -11,5 +14,8 @@ routes.get('/', (request, response) => {
 
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
